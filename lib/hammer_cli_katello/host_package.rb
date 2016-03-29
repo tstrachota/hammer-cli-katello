b0e06c466485f4ce5a18de7a3b454e9abc984e75
Tomas Strachota <tstrachota@redhat.com>
2016-03-29 17:53:49 +0200

    Experiment

--- lib/hammer_cli_katello/host_package.rb
+++ lib/hammer_cli_katello/host_package.rb
@@ -1,3 +1,10 @@
+class String
+  def remove_first_line!
+    first_newline = (index("\n") || size - 1) + 1
+    slice!(0, first_newline).sub("\n",'')
+  end
+end
+
 module HammerCLIKatello
 
   class HostPackage < HammerCLIKatello::Command
@@ -23,6 +30,73 @@ module HammerCLIKatello
 
       validate_options do
         option(:option_packages).required
+      end
+
+      def progress_bar
+        bar                                      = PowerBar.new
+        @closed = false
+        bar.settings.tty.finite.template.main    = '[${<bar>}] [${<percent>%}]'
+        bar.settings.tty.finite.template.padchar = ' '
+        bar.settings.tty.finite.template.barchar = '.'
+        # bar.settings.tty.finite.output           = Proc.new { |s| $stderr.print s }
+        # bar.settings.tty.finite.wipe             = "\e[0m\e[1G\e[K"
+        # bar.settings.tty.finite.template.wipe             = "\e[0m"
+        # bar.settings.tty.finite.template.post             = "\e[3A\e[K"
+        yield bar
+      ensure
+        bar.close
+      end
+
+      class Message
+        def initialize
+          @printed = false
+          @lines_count = 0
+        end
+
+        def show(msg)
+          puts eraser if @printed
+
+          cnt = msg.split("\n").count + 1
+          @lines_count = cnt if cnt > @lines_count
+
+          spacer = "\n" * (@lines_count - cnt)
+
+          puts "#{spacer}#{msg}"
+          @printed = true
+        end
+
+        def eraser
+          ("\e[1G\e[K\e[1A" * @lines_count)
+        end
+      end
+
+      def execute
+        progress_bar do |bar|
+          progress = 0.0
+          begin
+            str = "Message\non multiple\nlines\n"
+            msg = Message.new
+            while true
+
+
+              msg.show(str)
+
+              bar.show(:msg => msg, :done => progress, :total => 1)
+              sleep 1
+              progress += 0.1
+
+              break if progress > 1
+              if progress > 0.39
+                str.remove_first_line!
+              else
+                str += "lines #{progress}\n"
+              end
+            end
+          rescue Interrupt
+            # Inerrupting just means we stop rednering the progress bar
+          end
+        end
+        HammerCLI::EX_OK
       end
 
       build_options :without => [:groups]
